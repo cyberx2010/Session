c5c370dacccc430cf52c75127c2af148fbe767ff
Hiranya Sathsara <cyberxbot3@gmail.com>
2025-07-12 10:50:03 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -10,7 +10,7 @@ const {
     makeCacheableSignalKeyStore,
     Browsers,
     jidNormalizedUser
-} = require("baileys");
+} = require("whiskeysockets/baileys");
 const { upload } = require('./mega');
 
 function removeFile(FilePath) {
